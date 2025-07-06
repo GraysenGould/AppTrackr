f7964274e2ac67039b57c1aa25930902661c2fd3
Graysen Gould <gouldgraysen@gmail.com>
2025-07-06 17:33:53 -0500

    added route components

--- apptrackr.client/src/app/app.module.ts
+++ apptrackr.client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewAllComponent } from './tracking/view-all/view-all.component';
+import { SummaryComponent } from './tracking/summary/summary.component';
+import { CreateComponent } from './tracking/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewAllComponent,
+    SummaryComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ apptrackr.client/src/app/tracking/view-all/view-all.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all',
+  standalone: false,
+  templateUrl: './view-all.component.html',
+  styleUrl: './view-all.component.css'
+})
+export class ViewAllComponent {
+
+}
